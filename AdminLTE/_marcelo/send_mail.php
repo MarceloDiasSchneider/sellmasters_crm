525b7c31f4165263b57b27afc83e7de30dd110ea
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-03-30 21:24:23 +0200

    Update send_mail.php

--- AdminLTE/_marcelo/send_mail.php
+++ AdminLTE/_marcelo/send_mail.php
@@ -1,5 +1,9 @@
 <?php
 // The message
-$message = "Fare clic <a href='gasfacil.app.br/teste/autenticazione/change-password.php?code=123'>qui</a> per modificare la password";
+$message = "
+<html>
+    <p>Fare clic <a href='gasfacil.app.br/teste/autenticazione/change-password.php?code=123'>qui</a> per modificare la password</p>
+</html>    
+    ";
 
 echo mail('marcelo.d.schneider@gasfacil.app.br', 'Uma mensagem', $message);

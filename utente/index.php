637a95410401d0eb905bdd1adb0ed08122071185
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-03-31 16:24:18 +0200

    Update index.php

--- utente/index.php
+++ utente/index.php
@@ -46,80 +46,82 @@
 									</div>
 								</div>
 								<!-- /.card-header -->
-								<!-- form start -->
-								<form action='#' id='nuovo_utente' name='nuovo_utente' method='post'>
-									<div class="card-body">
-										<div class="row">
-											<div class="col-md-4">
-												<div class="form-group">
-													<label for="Nome">Nome</label>
-													<input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" maxlength="30" required>
+								<div class="card-body">
+									<!-- form start -->
+									<form action='#' id='nuovo_utente' name='nuovo_utente' method='post'>
+										<div class="card-body">
+											<div class="row">
+												<div class="col-md-4">
+													<div class="form-group">
+														<label for="Nome">Nome</label>
+														<input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" maxlength="30" required>
+													</div>
+												</div>
+												<div class="col-md-4">
+													<div class="form-group">
+														<label for="Cognome">Cognome</label>
+														<input type="text" class="form-control" id="cognome" name="cognome" placeholder="Cognome" maxlength="30">
+													</div>
+												</div>
+												<div class="col-md-4">
+													<div class="form-group">
+														<label for="data-nascita">Data di nascita</label>
+														<input type="date" class="form-control" id="data_nascita" name="data_nascita" placeholder="Data di nascita">
+													</div>
 												</div>
 											</div>
-											<div class="col-md-4">
-												<div class="form-group">
-													<label for="Cognome">Cognome</label>
-													<input type="text" class="form-control" id="cognome" name="cognome" placeholder="Cognome" maxlength="30">
+											<div class="row">
+												<div class="col-md-4">
+													<div class="form-group">
+														<label for="codiceFiscale">Codice Fiscale</label>
+														<input type="text" class="form-control" id="codice_fiscale" name="codice_fiscale" placeholder="Codice Fiscale" pattern="^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$" title="Codice fiscale errato." maxlength="16">
+													</div>
+												</div>
+												<div class="col-md-4">
+													<div class="form-group">
+														<label for="telefono">Telefono</label>
+														<input type="text" class="form-control" id="telefono" name="telefono" placeholder="Telefono" maxlength="16">
+													</div>
+												</div>
+												<div class="col-md-4">
+													<div class="form-group">
+														<label>Livello</label>
+														<select id="livello" name="livello" class="form-control" required>
+															<!-- <option disabled selected></option> -->
+														</select>
+													</div>
 												</div>
 											</div>
-											<div class="col-md-4">
-												<div class="form-group">
-													<label for="data-nascita">Data di nascita</label>
-													<input type="date" class="form-control" id="data_nascita" name="data_nascita" placeholder="Data di nascita">
+											<div class="row">
+												<div class="col-md-4">
+													<div class="form-group">
+														<label for="email">Email</label>
+														<input type="email" class="form-control" id="email" name="email" placeholder="Email" maxlength="50" required>
+													</div>
+												</div>
+												<div class="col-md-4">
+													<div class="form-group">
+														<label for="password">Password</label>
+														<input type="password" class="form-control" id="password" name="password" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Deve contenere almeno un numero, una lettera maiuscola, minuscola e almeno 8 caratteri." required>
+													</div>
+												</div>
+												<div class="col-md-4">
+													<div class="form-group">
+														<label for="verificaPassword">Verifica password</label>
+														<input type="password" class="form-control" id="verificaPassword" name="verificaPassword" placeholder="Verifica password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Deve contenere almeno un numero, una lettera maiuscola, minuscola e almeno 8 caratteri." required>
+													</div>
 												</div>
 											</div>
 										</div>
-										<div class="row">
-											<div class="col-md-4">
-												<div class="form-group">
-													<label for="codiceFiscale">Codice Fiscale</label>
-													<input type="text" class="form-control" id="codice_fiscale" name="codice_fiscale" placeholder="Codice Fiscale" pattern="^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$" title="Codice fiscale errato." maxlength="16">
-												</div>
-											</div>
-											<div class="col-md-4">
-												<div class="form-group">
-													<label for="telefono">Telefono</label>
-													<input type="text" class="form-control" id="telefono" name="telefono" placeholder="Telefono" maxlength="16">
-												</div>
-											</div>
-											<div class="col-md-4">
-												<div class="form-group">
-													<label>Livello</label>
-													<select id="livello" name="livello" class="form-control" required>
-														<!-- <option disabled selected></option> -->
-													</select>
-												</div>
-											</div>
+										<input type="hidden" id="attivo" name="attivo" value="1">
+										<input type="hidden" id="codiceSessione" name="codiceSessione" value="<?php echo $_SESSION['codiceSessione'] ?>">
+										<!-- /.card-body -->
+										<div class="card-footer">
+											<button type="submit" id="bottone_registra_utente" class="btn btn-primary">Registra</button>
+											<button type="submit" id="bottone_nuovo_utente" class="btn btn-primary d-none">Indietro a nuovo utente</button>
 										</div>
-										<div class="row">
-											<div class="col-md-4">
-												<div class="form-group">
-													<label for="email">Email</label>
-													<input type="email" class="form-control" id="email" name="email" placeholder="Email" maxlength="50" required>
-												</div>
-											</div>
-											<div class="col-md-4">
-												<div class="form-group">
-													<label for="password">Password</label>
-													<input type="password" class="form-control" id="password" name="password" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Deve contenere almeno un numero, una lettera maiuscola, minuscola e almeno 8 caratteri." required>
-												</div>
-											</div>
-											<div class="col-md-4">
-												<div class="form-group">
-													<label for="verificaPassword">Verifica password</label>
-													<input type="password" class="form-control" id="verificaPassword" name="verificaPassword" placeholder="Verifica password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Deve contenere almeno un numero, una lettera maiuscola, minuscola e almeno 8 caratteri." required>
-												</div>
-											</div>
-										</div>
-									</div>
-									<input type="hidden" id="attivo" name="attivo" value="1">
-									<input type="hidden" id="codiceSessione" name="codiceSessione" value="<?php echo $_SESSION['codiceSessione'] ?>">
-									<!-- /.card-body -->
-									<div class="card-footer">
-										<button type="submit" id="bottone_registra_utente" class="btn btn-primary">Registra</button>
-										<button type="submit" id="bottone_nuovo_utente" class="btn btn-primary d-none">Indietro a nuovo utente</button>
-									</div>
-								</form>
+									</form>
+								</div>
 							</div>
 							<!-- /.card -->
 						</div>

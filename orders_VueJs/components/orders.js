f335c08c4f11144d78ca5fd12951a6b3135fe332
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-04-23 17:46:59 +0200

    a

--- orders_VueJs/components/orders.js
+++ orders_VueJs/components/orders.js
@@ -5,7 +5,7 @@ app.component('orders', {
             <div class="col-md-12">
                 <div class="card card-primary">
                     <div class="card-header">
-                        <h3 class="card-title" @click="log">Gestione di Ordini</h3>
+                        <h3 class="card-title">Gestione di Ordini</h3>
                         <div class="card-tools">
                             <button type="button" class="btn btn-tool" data-card-widget="collapse">
                                 <i class="fas fa-minus"></i>
@@ -14,8 +14,8 @@ app.component('orders', {
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
-                        <form action='#' id='user_form' name='user_form' method='post' @submit.prevent="get_data">
-                            <div class="row">
+                        <form action='#' id='user_form' name='user_form' method='post' @submit.prevent="get_orders">
+                            <div class="row" v-show="!datatables">
                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="start_date">Data inizio</label>
@@ -43,7 +43,7 @@ app.component('orders', {
                                     <div class="float-sm-right ml-1 mt-1" v-show="!datatables">
                                         <button type="submit" class="btn btn-primary">Ricerca</button>
                                     </div>
-                                    <div class="float-sm-right ml-1 mt-1" v-show="datatables">
+                                    <div class="float-sm-right ml-1 mt-1" v-show="false">
                                         <button type="submit" class="btn btn-primary" @click.prevent="refresh_datatables">Ricaricare</button>
                                     </div>
                                 </div>
@@ -51,7 +51,7 @@ app.component('orders', {
                         </form>
                         <hr>
                         <!-- table -->
-                        <table id="orders" class="table table-bordered table-striped">
+                        <table id="orders" class="table table-bordered table-striped nowrap">
                         </table>
                     </div>
                     <!-- /.card-body -->
@@ -63,12 +63,13 @@ app.component('orders', {
             </div>
             <!-- /.tabella con tutti gli utente registrate -->
         </div>
+        {{columns_data}}
         <!-- /.row -->`,
     data() {
         return {
             // variables to bind the form
-            startDate: '2021-04-20',
-            endDate: '2021-04-21',
+            startDate: '2021-03-15',
+            endDate: '2021-03-16',
             merchants_id: '',
             // array to hold the select options
             select_options: [],
@@ -76,6 +77,9 @@ app.component('orders', {
             datatables: false,
             // variable to control the loading card
             loading: false,
+            // variable to hold the orders data
+            orders_data: null,
+            columns_data: [],
         }
     },
     methods: {
@@ -116,58 +120,74 @@ app.component('orders', {
                     console.error('There was an error!', error);
                 });
         },
+        // get json from backend
+        get_orders() {
+            let self = this
+            let dates = 'data_inizio=' + this.startDate + '&data_fine=' + this.endDate
+            $.ajax({
+                type: "POST",
+                url: "http://51.91.97.200/sellmaster/api_sellmasters/ordini_mondotop.php",
+                data: dates,
+                dataType: "json",
+                async: false,
+                success: function(data) {
+                    self.orders_data = data
+                    self.set_header_titles()
+                    self.set_market_status_color()
+                    self.set_datatables()
+                },
+                error: function(msg) {
+                    alert("Failed: " + msg.status + ": " + msg.statusText);
+                }
+            });
+        },
+        // define the title on datatables header 
+        set_header_titles() {
+            let header_titles = Object.keys(this.orders_data[0]);
+            header_titles.forEach(title => {
+                let Title = title.replaceAll("_", " ").toUpperCase();
+                this.columns_data.push({ 'title': Title, 'data': title, })
+            });
+        },
+        // set a color to each merket status 
+        set_market_status_color() {
+            this.orders_data.forEach(order => {
+                switch (order.market_status) {
+                    case 'Pending':
+                        order.market_status = '<span class="bg-info px-3">' + order.market_status + '</span>'
+                        break;
+                    case 'Unhipped':
+                        order.market_status = '<span class="bg-orange px-3">' + order.market_status + '</span>'
+                        break;
+                    case 'Shipped':
+                        order.market_status = '<span class="bg-success px-3">' + order.market_status + '</span>'
+                        break;
+                    case 'Cancelled':
+                        order.market_status = '<span class="bg-danger px-3">' + order.market_status + '</span>'
+                        break;
+                    default:
+                        break;
+                }
+            });
+        },
         // initiate the dataTables
-        get_data() {
+        set_datatables() {
             this.datatables = true
             let self = this
             $("#orders").DataTable({
-                'ajax': {
-                    type: "POST",
-                    url: "http://51.91.97.200/sellmaster/api_sellmasters/ordini_mondotop.php",
-                    data: function() {
-                        return { 'data_inizio': self.startDate, 'data_fine': self.endDate }
-                    },
-                    dataType: "json",
-                    async: false,
-                    dataSrc: ""
-                },
-                "dom": 'BlrtipRS',
-                // R = colReorder: true,
-                // B = button: true,
-                "scrollX": true,
+                "data": self.orders_data,
+                "dom": '<"row mb-2"<"col-sm-12 col-md-8"B><"col-sm-12 col-md-4"f>><"row mb-2"<"col-sm-12"rt>><"row mb-2"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
+                // set the inicial page length
+                "pageLength": 25,
+                // desable responsive to activete the scroll X
                 "responsive": false,
-                "scrollCollapse": false,
-                "lengthChange": true,
-                "pageLength": 20,
-                columns: [
-                    { title: "ID ordine", data: "order_id" },
-                    { title: "Carrier", data: "carrier" },
-                    { title: "Market Status", data: "market_status" },
-                    { title: "Data di acquisto", data: "purchase_date" },
-                    { title: "Prezzo articolo", data: "item_price" },
-
-                    { title: "ID ordine", data: "order_id" },
-                    { title: "Carrier", data: "carrier" },
-                    { title: "Market Status", data: "market_status" },
-                    { title: "Data di acquisto", data: "purchase_date" },
-                    { title: "Prezzo articolo", data: "item_price" },
-
-                    { title: "ID ordine", data: "order_id" },
-                    { title: "Carrier", data: "carrier" },
-                    { title: "Market Status", data: "market_status" },
-                    { title: "Data di acquisto", data: "purchase_date" },
-                    { title: "Prezzo articolo", data: "item_price" },
-
-                    { title: "ID ordine", data: "order_id" },
-                    { title: "Carrier", data: "carrier" },
-                    { title: "Market Status", data: "market_status" },
-                    { title: "Data di acquisto", data: "purchase_date" },
-                    { title: "Prezzo articolo", data: "item_price" },
-                ],
+                "scrollX": true,
+                // "lengthChange": true,
+                columns: self.columns_data,
                 "lengthChange": false,
                 "autoWidth": true,
                 "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-            }).buttons().container().appendTo('#orders_wrapper .col-md-6:eq(0)');
+            }).buttons().container().appendTo('#orders_wrapper .col-md-8:eq(0)');
         },
         // refresh the datatables
         refresh_datatables() {
@@ -178,12 +198,9 @@ app.component('orders', {
                 false
             );
         },
-        log() {
-            console.log(this.startDate);
-            console.log(this.endDate);
-        }
     },
     mounted() {
         this.get_select_options()
+            // this.get_orders()
     }
 })
---
+++ orders_VueJs/orders_manipulator_class.php
@@ -0,0 +1,124 @@
+<?php
+
+$merchant_id = $_REQUEST['merchant_id'];
+$startDate = $_REQUEST['startDate'];
+$endDate = $_REQUEST['endDate'];
+$url = 'http://51.91.97.200/sellmaster/api_sellmasters/ordini_mondotop.php';
+
+class ordersManipulatorClass
+{
+    public $url;
+    public $startDate;
+    public $endDate;
+    public $merchant_id;
+
+    function __construct($url, $merchant_id, $startDate, $endDate)
+    {
+        $this->url = $url;
+        $this->merchant_id  = $merchant_id;
+        $this->startDate  = $startDate;
+        $this->endDate = $endDate;
+    }
+
+    // get all orders
+    public function get_orders()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->url . '?data_inizio=' . $this->startDate . '&data_fine=' . $this->endDate,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return json_decode($response, true);
+    }
+
+    // check if dati_finanziari iqual a refund
+    public function dati_finanziari_refund($orders)
+    {
+        foreach ($orders as $key => $order) {
+            $found = false;
+            foreach ($order['dati_finanziari'] as $key => $value) {
+                if ($value['type'] == 'RefundCommission') {
+                    $found = true;
+                }
+            }
+            if ($found) {
+                $order['market_status'] = 'Refund';
+            }
+            $ordersRefund[] = $order;
+        }
+        return $ordersRefund;
+    }
+
+    // set the fomataion foreach market_status 
+    public function market_status_formation($orders)
+    {
+        foreach ($orders as $key => $order) {
+            switch ($order['market_status']) {
+                case 'Pending':
+                    $order['market_status'] = '<span class="bg-info px-3">' . $order['market_status'] . '<\/span>';
+                    break;
+                case 'Unhipped':
+                    $order['market_status'] = '<span class="bg-orange px-3">' . $order['market_status'] . '<\/span>';
+                    break;
+                case 'Shipped':
+                    $order['market_status'] = '<span class="bg-success px-3">' . $order['market_status'] . '<\/span>';
+                    break;
+                case 'Cancelled':
+                    $order['market_status'] = '<span class="bg-lime px-3">' . $order['market_status'] . '<\/span>';
+                    break;
+                case 'Refund':
+                    $order['market_status'] = '<span class="bg-danger px-3">' . $order['market_status'] . '<\/span>';
+                    break;
+                default:
+                    break;
+            }
+            $ordersFormated[] = $order;
+        }
+        return $ordersFormated;
+    }
+    // item_price shipping_prici item_promotion_discount 
+    public function total_order($orders)
+    {
+        foreach ($orders as $key => $order) {
+            // use floatval to avoid non-numeric warning
+            $item_price = floatval($order['item_price']);
+            $shipping_price = floatval($order['shipping_price']);
+            $item_promotion_discount = floatval($order['item_promotion_discount']);
+            $total = $item_price + $shipping_price + $item_promotion_discount;
+            $order['total_order'] =  $total;
+            $totalOrder[] = $order;
+        }
+        return $totalOrder;
+    }
+    // GBP currency convert fi 
+    public function currency_convert($orders)
+    {
+        foreach ($orders as $key => $order) {
+            if ($order['currency'] == 'GBP') {
+                $order['total_order'] = $order['total_order'] / 0.9;
+            }
+            $currencyUpdated[] = $order;
+        }
+        return $currencyUpdated;
+    }
+}
+
+$order = new ordersManipulatorClass($url, $merchant_id, $startDate, $endDate);
+$orders = $order->get_orders();
+$ordersRefund = $order->dati_finanziari_refund($orders);
+$ordersFormated = $order->market_status_formation($ordersRefund);
+$totalOrder = $order->total_order($ordersFormated);
+$currencyUpdated = $order->currency_convert($totalOrder);
+
+echo json_encode($currencyUpdated);
